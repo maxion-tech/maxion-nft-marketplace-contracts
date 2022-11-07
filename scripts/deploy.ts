e7e5ad77ac43430d5639e5b743572ec578ff1d29
Ton Maxion <ton@maxion.tech>
2022-11-07 14:49:40 +0700

    add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,44 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
+import { utils } from "ethers";
+import { getImplementationAddress } from "@openzeppelin/upgrades-core";
+import { MaxionNFTMarketplace } from "../typechain-types";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // Environment variable validation
+  const { NFT_ADDRESS, CURRENCY_TOKEN_ADDRESS, PLATFORM_TREASURY_ADDRESS, PARTNER_ADDRESS } = process.env;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  if (!NFT_ADDRESS || !CURRENCY_TOKEN_ADDRESS || !PLATFORM_TREASURY_ADDRESS || !PARTNER_ADDRESS) throw new Error("Environment variable not valid");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const totalFeePercent = "1000000000"; // 10%*10**8 (10**18-10**10) // Total fee 10%
+  const platformFeePercent = "6000000000"; // 60%*10**8 (10**18-10**10)
+  const partnerFeePercent = "4000000000"; // 40%*10**8 (10**18-10**10)
+  const minimumTradePrice = utils.parseEther("1");
 
-  await lock.deployed();
+  const NFTMarketplace = await ethers.getContractFactory(
+    "MaxionNFTMarketplace"
+  );
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  const nftMarketplace = (await upgrades.deployProxy(NFTMarketplace, [
+    NFT_ADDRESS, // NFT address
+    CURRENCY_TOKEN_ADDRESS, // Currency token address
+    PLATFORM_TREASURY_ADDRESS, // Platform treasury wallet addfress
+    PARTNER_ADDRESS, // Partner wallet address
+    totalFeePercent, // Total fee percent,
+    platformFeePercent, // Partner fee
+    partnerFeePercent, // Platform fee,
+    minimumTradePrice, // Minimum trade price
+  ])) as MaxionNFTMarketplace;
+
+  await nftMarketplace.deployed();
+
+  const nftMarketplaceImplAddress = await getImplementationAddress(
+    ethers.provider,
+    nftMarketplace.address
+  );
+
+  console.log(`Maxion NFT Marketplace deployed to: ${nftMarketplace.address}`);
+  console.log(`Implementation address is: ${nftMarketplaceImplAddress}`);
+  console.log(`Verify contract by command: npx hardhat verify ${nftMarketplace.address} --network ??`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
